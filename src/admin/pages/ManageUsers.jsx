5a96b5ee57dcbb555e51679f39f7441e54955cbb
Farseen247 <farseen247@gmail.com>
2024-12-22 20:37:37 +0530

    adminside

--- src/admin/pages/ManageUsers.jsx
+++ src/admin/pages/ManageUsers.jsx
@@ -8,7 +8,7 @@ const ManageUsers = () => {
     useEffect(() => {
         getAllUsers().then((res) => setUsers(res.data));
     }, []);
-
+//
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-6">Manage Users</h1>
