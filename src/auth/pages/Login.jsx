895b79e7ef3bc030d95bbb88be1c040a8014f0b9
Farseen247 <farseen247@gmail.com>
2024-12-25 17:13:31 +0530

    updated-login

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
 
 const Login = () => {
-  const { handleLogin } = useUser(); // Use handleLogin for logging in
-  const navigate = useNavigate();
+  const { handleLogin } = useUser();
   const [form, setForm] = useState({ email: "", password: "" });
   const [message, setMessage] = useState("");
 
@@ -29,41 +28,16 @@ const Login = () => {
     <div className="flex justify-center h-screen w-screen items-center">
       <div className="flex flex-col items-center w-[350px] h-auto rounded-lg p-4 border shadow-lg">
         <h1 className="font-bold text-3xl mt-5 font-serif">Login</h1>
-        <form className="p-6 w-80" onSubmit={handleSubmit}>
-          {message && <p className="text-red-500 text-center">{message}</p>}
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            className="font-serif mt-5 border py-2 w-[235px] outline-none pl-2"
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            className="font-serif mt-5 border py-2 w-[235px] outline-none pl-2"
-            value={form.password}
-            onChange={handleChange}
-            required
-          />
-          <NavLink className="mr-32 mt-5 text-sm text-gray-400 font-serif">
-            Forget Password?
-          </NavLink>
-          <button
-            type="submit"
-            className="bg-blue-600 hover:bg-blue-700 mt-5 text-white py-2 px-16 rounded-full font-semibold ml-[50px]"
-          >
-            Login
-          </button>
+        <form className="p-6 text-center w-80" onSubmit={handleSubmit}>
+            {message && <p className="text-red-500 text-center">{message}</p>}
+            <input type="email" placeholder="Email" name="email" className="font-serif mt-5 border py-2 w-[235px] outline-none pl-2" value={form.email} onChange={handleChange} required />
+            <input type="password" placeholder="Password" name="password" className="font-serif mt-5 border py-2 w-[235px] outline-none pl-2" value={form.password} onChange={handleChange} required/>
+            <NavLink ><p className="mr-32 text-sm text-gray-400 font-seri mt-2" >Forget Password?</p></NavLink>
+            <button type="submit" className="bg-blue-600 hover:bg-blue-700 mt-5 text-white py-2 px-16 rounded-full font-semibold">Login</button>
         </form>
         <div className="flex font-serif text-sm text-gray-500">
           <p className="mt-2">Not a Member? </p>
-          <NavLink to={"/signup"}>
-            <h1 className="mt-2 text-blue-600">Sign up</h1>
-          </NavLink>
+          <NavLink to={"/signup"}><h1 className="mt-2 text-blue-600">Sign up</h1></NavLink>
         </div>
       </div>
     </div>
